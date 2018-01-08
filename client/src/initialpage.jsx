fe1a37beefbb4f6596397926fc8163e7c094d22e
Manik Sewak <Maniks04@aol.com>
2018-01-07 19:51:54 -0500

    test

--- client/src/initialpage.jsx
+++ client/src/initialpage.jsx
@@ -5,24 +5,38 @@ class Initialpage extends React.Component {
     constructor(props) {
         super(props)
 
+        this.state = {
+            startClicked : true,
+            countdown : 3
+        }
 
+    }
+
+    handleStartClick () {
+        console.log('hey')
+        //this.setState({startClicked : true})
     }
 
     
 
 
     render() {
-        return(
-            <div>
-                <h1 className='welcome' >WELCOME</h1>
-                <h3 className='to' >to</h3>
-                <h1 className='gameme' >GAME--ME</h1>
-                <form className='startbuttoncontainer'>
-                <button className='startbutton'>START</button>
-                </form>
-            </div>
-           
-        )
+        if (this.state.startClicked === true) {
+            return(<div className='fuckyou'>FUCK YOU</div>)
+        }
+        if (this.state.startClicked === false) {
+            return(
+                <div>
+                    <h1 className='welcome' >WELCOME</h1>
+                    <h3 className='to' >to</h3>
+                    <h1 className='gameme' >GAME--ME</h1>
+                    <form className='startbuttoncontainer'>
+                    <button onClick={(e) => {e.preventDefault(); this.handleStartClick}} className='startbutton'>START</button>
+                    </form>
+                </div>
+            )
+        }
+       
     }
 
 }
