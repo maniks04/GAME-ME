d98a33771e5bdcef880ec38479c43b95c0490f74
Manik Sewak <Maniks04@aol.com>
2018-01-07 19:08:37 -0500

    test

--- client/src/initialpage.jsx
+++ client/src/initialpage.jsx
@@ -15,6 +15,8 @@ class Initialpage extends React.Component {
         return(
             <div>
                 <h1 className='welcome' >WELCOME</h1>
+                <h1 className='to' >to</h1>
+                <h1 className='gameme' >GAME--ME</h1>
             </div>
         )
     }
